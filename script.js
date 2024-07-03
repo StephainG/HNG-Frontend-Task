3e84041d3e1991a4e1d5e5e98acca05eb4310c27
StephainG <onibukunoluwa@yahoo.com>
2024-07-03 10:29:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const handleDate = () => {
+    const now = new Date();
+    const watDate = new Date(now.getTime() + 1 * 60 * 60 * 1000);
+    const currentTimeWAT = watDate.toUTCString().replace('GMT', 'WAT');
+
+    const options = { weekday: 'long', timeZone: 'UTC' };
+    const currentDay = watDate.toLocaleDateString('en-US', options);
+
+    document.querySelector('[data-testid="currentTimeUTC"]').innerText = currentTimeWAT;
+    document.querySelector('[data-testid="currentDay"]').innerText = currentDay;
+}
+
+document.addEventListener('DOMContentLoaded', handleDate);
